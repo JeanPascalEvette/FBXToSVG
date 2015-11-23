dd5f0627522a9166b1050ef090a28a6f61fbcf33
Jeyp <jp.evette1@gmail.com>
2015-11-23 17:45:25 +0000

    Working on generating my own svg file

--- FBXReader/FBXReader/FBXReader.py
+++ FBXReader/FBXReader/FBXReader.py
@@ -35,16 +35,58 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     
     s.wfile.write("<p>You accessed path: %s</p>" % s.path)
     s.wfile.write("<p>Vertices Count: %s</p>" % getVerticesCount(s.path))
-    dwg = svgwrite.Drawing('test.svg', profile='tiny')
-    dwg.add(dwg.line((0, 0), (10, 0), stroke=svgwrite.rgb(10, 10, 16, '%')))
-    dwg.add(dwg.text('Test', insert=(0, 0.2), fill='red'))
-    dwg.save()
+    
+    drawLines(s.path)
+
     s.wfile.write("<object data=\"test.svg\" type=\"image/svg+xml\"></object>")
     s.wfile.write("</body></html>")
     
 
+def drawLines(pathToFbx):
+    sdk_manager, scene = FbxCommon.InitializeSdkObjects()
+    if not FbxCommon.LoadScene(sdk_manager, scene, os.getcwd()+pathToFbx):
+        print("error in LoadScene. File found : %s" % os.path.isfile(pathToFbx))
+        print("Current working directory : %s" % os.getcwd())
+        print("file directory : %s" % os.getcwd()+pathToFbx)
+        
+    svgContents = "<?xml version='1.0' encoding='UTF-8' standalone='no'?>\n<!DOCTYPE svg PUBLIC '-//W3C//DTD SVG 1.0//EN' 'http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd'>\n<svg width='280' height='280'\nxmlns='http://www.w3.org/2000/svg'\nxmlns:xlink='http://www.w3.org/1999/xlink'\nonload='init(evt)' >\n<script type='text/ecmascript'> \n<![CDATA[\n"
+    for u in range(scene.GetNodeCount()):
+        node = scene.GetNode(u)
+        for i in range(node.GetChildCount()):
+            child = node.GetChild(i)
+            svgContents += "edges = ["
+            for m in range(child.GetMesh().GetPolygonCount()):
+                svgContents += "["
+                for n in range(child.GetMesh().GetPolygonSize(m)):
+                    svgContents += str(child.GetMesh().GetPolygonVertex(m,n))+","
+                svgContents = svgContents[-1:] + "],"
+            svgContents = svgContents[-1:] + "]\n"
+
+        
+            xCoords = "["
+            yCoords = "["
+            zCoords = "["
+            for i in range(child.GetMesh().GetControlPointsCount()):
+                xCoords += str(child.GetMesh().GetControlPoints()[i][0]) + ","
+                yCoords += str(child.GetMesh().GetControlPoints()[i][1]) + ","
+                zCoords += str(child.GetMesh().GetControlPoints()[i][2]) + ","
+            
+            xCoords = xCoords[-1:] + "];\n"
+            yCoords = yCoords[-1:] + "];\n"
+            zCoords = zCoords[-1:] + "];\n"
+
+            svgContents += xCoords + yCoords + zCoords
+
+
+    with open('myFile.svg', 'w') as file_:
+        file_.write(svgContents)
+    
+    
+
 def getVerticesCount(pathToFbx):
     sdk_manager, scene = FbxCommon.InitializeSdkObjects()
+    
+
     if not FbxCommon.LoadScene(sdk_manager, scene, os.getcwd()+pathToFbx):
         print("error in LoadScene. File found : %s" % os.path.isfile(pathToFbx))
         print("Current working directory : %s" % os.getcwd())
@@ -60,19 +102,32 @@ def getVerticesCount(pathToFbx):
             counter += child.GetMesh().GetPolygonVertexCount()
             lineCounter += child.GetMesh().GetMeshEdgeCount()
             child.GetMesh().BeginGetMeshEdgeVertices()
+            polCOunt = ""
             for z in range(child.GetMesh().GetMeshEdgeCount()):
                 startIndex = -1
                 endIndex = -1
                 startIndex, endIndex = child.GetMesh().GetMeshEdgeVertices(z)
                 startVector = child.GetMesh().GetControlPointAt(startIndex)
                 endVector = child.GetMesh().GetControlPointAt(endIndex)
+                
+
+
+                polList = "<br/>"
+
+                for m in range(child.GetMesh().GetPolygonCount()):
+                    polList += "("
+                    for n in range(child.GetMesh().GetPolygonSize(m)):
+                        polList += str(child.GetMesh().GetControlPointAt(child.GetMesh().GetPolygonVertex(m,n)))+","
+                    polList += ")<br/>"
+
+
                 lineList += "<br/>" + str(startVector) + " - " + str(endVector)
             child.GetMesh().EndGetMeshEdgeVertices()
             attr_type = child.GetNodeAttribute().GetAttributeType()
 
             if attr_type == FbxCommon.FbxNodeAttribute.eMesh:
                 print(child)
-    return (str(counter) + " edge count " + str(lineCounter) + lineList)
+    return (str(counter) + " edge count " + str(lineCounter) + "-" + polCOunt + polList + lineList )
     
 
 httpd = BaseHTTPServer.HTTPServer(("localhost", 8000), MyHandler)
