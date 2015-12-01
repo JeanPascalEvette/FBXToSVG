73c5b49519b6a1dcee6ca8b45d3783e8d5b70a64
Jeyp <jp.evette1@gmail.com>
2015-12-01 16:42:34 +0000

    Implemented reading multiple files and displaying thumbnails

--- FBXReader/FBXReader/FBXReader.py
+++ FBXReader/FBXReader/FBXReader.py
@@ -1,4 +1,5 @@
 ﻿import FbxCommon
+import math
 import svgwrite
 
 
@@ -12,7 +13,7 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
 
   def do_GET(s):
-
+      
     if s.path.endswith(".svg"):
                         f=open(os.getcwd()+s.path)
                         s.send_response(200)
@@ -21,6 +22,11 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
                         s.wfile.write(f.read())
                         f.close()
                         return
+    if s.path.endswith(".ico"):
+                        s.send_response(404)
+                        s.send_header('Content-type',        'image/ico')
+                        s.end_headers()
+                        return
 
     if(s.path == "/favicon.ico" or s.path == "/test.svg"):
         return
@@ -28,34 +34,31 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     s.send_response(200)
     s.send_header("Content-type", "text/html")
     s.end_headers()
-    s.wfile.write("<html><head><title>Title goes here.</title></head>")
-    s.wfile.write("<body><p>This is a test.</p>")
-
-
+    s.wfile.write("<html><head><title>Title goes here.</title>")
+    s.wfile.write("<style type='text/css'>object\n{\nborder : 1px inset lightgray;\n}\n.thumbnail\n{\nmargin-right:10px;\nmargin-bottom:10px;\n}\n</style></head>")
+    s.wfile.write("<body>")
     
-    s.wfile.write("<p>You accessed path: %s</p>" % s.path)
-    s.wfile.write("<p>Vertices Count: %s</p>" % getVerticesCount(s.path))
     
-    drawLines(s.path)
-
-    s.wfile.write("<object data=\"test.svg\" type=\"image/svg+xml\"></object>")
+    for dirname, dirnames, filenames in os.walk('FBX'):
+        for filename in filenames:
+            drawLines("/FBX/"+filename)
+            s.wfile.write("<object class='thumbnail' width='150px' height='150px' data=\"SVG/"+filename[:-3]+"svg\" type=\"image/svg+xml\"></object>")
     s.wfile.write("</body></html>")
     
 
 def drawLines(pathToFbx):
-
     rotation =  45 
     rotation = rotation * 3.1415926 / 180
 
     sdk_manager, scene = FbxCommon.InitializeSdkObjects()
-    if not FbxCommon.LoadScene(sdk_manager, scene, os.getcwd()+pathToFbx):
+    if not FbxCommon.LoadScene(sdk_manager, scene, os.getcwd()+pathToFbx ):
         print("error in LoadScene. File found : %s" % os.path.isfile(pathToFbx))
         print("Current working directory : %s" % os.getcwd())
         print("file directory : %s" % os.getcwd()+pathToFbx)
         
-    svgContents = "<?xml version='1.0' encoding='UTF-8' standalone='no'?>\n<!DOCTYPE svg PUBLIC '-//W3C//DTD SVG 1.0//EN' 'http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd'>\n<svg width='280' height='280'\nxmlns='http://www.w3.org/2000/svg'\nxmlns:xlink='http://www.w3.org/1999/xlink'\nonload='init(evt)' >"
+    svgContents = "<?xml version='1.0' encoding='UTF-8' standalone='no'?>\n<!DOCTYPE svg PUBLIC '-//W3C//DTD SVG 1.0//EN' 'http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd'>\n<svg width='100%' height='100%' viewBox='0 0 0 0' preserveAspectRatio='xMidYMid meet' \nxmlns='http://www.w3.org/2000/svg'\nxmlns:xlink='http://www.w3.org/1999/xlink'\nonload='init(evt)'>"
 
-    svgContents += "\n\n<style>\n\tsvg\n{\nmargin-left:100px;\nmargin-top:100px;\n}\n.edge{\n\t\tstroke: black;\n\t\tstroke-width: 1;\n\t}\n\t.button{\n\t\tfill: #2060dd;\n\t\tstroke: #2580ff;\n\t\tstroke-width: 1;\n\t}\n\t.button:hover{\n\t\tstroke-width: 3;\n\t}\n</style>"
+    svgContents += "\n\n<style>\n\tsvg\n{\n}\n.edge{\n\t\tstroke: black;\n\t\tstroke-width: 1;\n\t}\n\t.button{\n\t\tfill: #2060dd;\n\t\tstroke: #2580ff;\n\t\tstroke-width: 1;\n\t}\n\t.button:hover{\n\t\tstroke-width: 3;\n\t}\n</style>"
 
     svgContents += "\n<script type='text/ecmascript'> \n<![CDATA[\n"
 
@@ -77,26 +80,32 @@ def drawLines(pathToFbx):
                     svgContents += str(child.GetMesh().GetPolygonVertex(m,n))+","
                 svgContents = svgContents[:-1] + "],"
             svgContents = svgContents[:-1] + "]\n"
-
-        
+                
+            svgContents += "depth = ["
+            for m in range(child.GetMesh().GetPolygonCount()):
+                svgContents += "0,"
+            svgContents = svgContents[:-1] + "];\n"
 
         
             xCoords = "x_coords = ["
             yCoords = "y_coords = ["
             zCoords = "z_coords = ["
-            smallestControlPoint = 0
+            smallestControlPointX = 0
+            smallestControlPointY = 0
+            smallestControlPointZ = 0
             for i in range(child.GetMesh().GetControlPointsCount()):
-                if(smallestControlPoint > child.GetMesh().GetControlPoints()[i][0]):
-                    smallestControlPoint = child.GetMesh().GetControlPoints()[i][0]
-                if(smallestControlPoint > child.GetMesh().GetControlPoints()[i][1]):
-                    smallestControlPoint = child.GetMesh().GetControlPoints()[i][1]
-                if(smallestControlPoint > child.GetMesh().GetControlPoints()[i][2]):
-                    smallestControlPoint = child.GetMesh().GetControlPoints()[i][2]
-            smallestControlPoint -= 30
+                if(smallestControlPointX > child.GetMesh().GetControlPoints()[i][0]):
+                    smallestControlPointX = child.GetMesh().GetControlPoints()[i][0]
+                if(smallestControlPointY > child.GetMesh().GetControlPoints()[i][1]):
+                    smallestControlPointY = child.GetMesh().GetControlPoints()[i][1]
+                if(smallestControlPointZ > child.GetMesh().GetControlPoints()[i][2]):
+                    smallestControlPointZ = child.GetMesh().GetControlPoints()[i][2]
+
+
             for i in range(child.GetMesh().GetControlPointsCount()):
-                xCoords += str(child.GetMesh().GetControlPoints()[i][0] - smallestControlPoint) + ","
-                yCoords += str(child.GetMesh().GetControlPoints()[i][1] - smallestControlPoint) + ","
-                zCoords += str(child.GetMesh().GetControlPoints()[i][2] - smallestControlPoint) + ","
+                xCoords += str(child.GetMesh().GetControlPoints()[i][0] - smallestControlPointX) + ","
+                yCoords += str(child.GetMesh().GetControlPoints()[i][1] - smallestControlPointY) + ","
+                zCoords += str(child.GetMesh().GetControlPoints()[i][2] - smallestControlPointZ) + ","
             
             xCoords = xCoords[:-1] + "];\n"
             yCoords = yCoords[:-1] + "];\n"
@@ -104,9 +113,15 @@ def drawLines(pathToFbx):
 
             svgContents += xCoords + yCoords + zCoords
 
-            svgContents += "\n\ncentre_x = "+str(-smallestControlPoint)+";\ncentre_y = "+str(-smallestControlPoint)+";\ncentre_z = "+str(-smallestControlPoint)+";\n\n\n"
-            svgContents += "function init(evt)\n{\n\tif ( window.svgDocument == null )\n\t{\n\t\tsvgDocument = evt.target.ownerDocument;\n\t}\n\trotateAboutX("+str(rotation)+");\n\trotateAboutY("+str(rotation)+");\n\tdrawBox();\n}"
-            svgContents += "\n\n\nfunction drawBox()\n{\n\tfor(var i=0; i<faces.length; i++)\n\t{\n\t\tface = svgDocument.getElementById('face-'+i);\n\t\tface.setAttributeNS(null, 'd', 'm'+x_coords[faces[i][0]]+' '+y_coords[faces[i][0]] + ' ' + 'L'+x_coords[faces[i][1]]+' '+y_coords[faces[i][1]] + ' ' + 'L'+x_coords[faces[i][2]]+' '+y_coords[faces[i][2]] + ' ' + 'L'+x_coords[faces[i][3]]+' '+y_coords[faces[i][3]] + ' ' + 'Z');\n\t}\n}"
+            svgContents += "\n\ncentre_x = "+str(-smallestControlPointX)+";\ncentre_y = "+str(-smallestControlPointY)+";\ncentre_z = "+str(-smallestControlPointZ)+";\n\n\n"
+            svgContents += "function init(evt)\n{\n\tif ( window.svgDocument == null )\n\t{\n\t\tsvgDocument = evt.target.ownerDocument;\n\t}\n\trotateAboutX("+str(rotation)+");\n\trotateAboutY("+str(rotation)+");\n\tcalculateDepth()\n\tdrawBox();\nsetViewBox();\n}"
+            
+            svgContents += "function setViewBox()\n{\n\tvar minX = 0;\n\tvar minY = 0;\n\tvar maxX = 0;\n\tvar maxY = 0;\n\t\n\tfor(var i = 0; i < x_coords.length; i++)\n\t{\n\t\tif(minX == 0 || x_coords[i] < minX)\n\t\t\tminX = x_coords[i];\n\t\tif(minY == 0 || y_coords[i] < minY)\n\t\t\tminY = y_coords[i];\n\t\tif(maxX == 0 || x_coords[i] > maxX)\n\t\t\tmaxX = x_coords[i];\n\t\tif(maxY == 0 || y_coords[i] > maxY)\n\t\t\tmaxY = y_coords[i];\n\t}\n\tshape = document.getElementsByTagName('svg')[0];\n\tshape.setAttribute('viewBox', minX+' '+ minY+' '+ maxX +' '+maxY);\n}"
+            
+            svgContents += "\n\n\nfunction calculateDepth()\n{\n\tvar facesDepth = Array(faces.length);\n\tfor(var i = 0; i < faces.length; i++)\n\t{\n\t\tvar currentDepth = 0;\n\t\tfor(var u = 0; u < faces[i].length; u ++)\n\t\t{\n\t\t\tcurrentDepth += z_coords[faces[i][u]];\n\t\t}\n\t\tcurrentDepth /= faces[i].length;\n\t\tfacesDepth[i] = currentDepth;\n\t}\n\tfor(var i = 0; i < depth.length; i++)\n\t{\n\t\tvar smallest = -1;\n\t\tfor(var u = 0; u < facesDepth.length; u++)\n\t\t{\n\t\t\tif(facesDepth[u] != -99999 && (smallest == -1 || facesDepth[smallest] > facesDepth[u]))\n\t\t\t\tsmallest = u;\n\t\t}\n\t\tdepth[i] = smallest;\n\t\tfacesDepth[smallest] = -99999;\n\t}\n}"
+            svgContents += "\n\n\nfunction drawBox()\n{\n\tfor(var i=0; i<depth.length; i++)\n\t{\n\t\tface = svgDocument.getElementById('face-'+i);\n\t\tvar d = 'm'+x_coords[faces[depth[i]][0]]+' '+y_coords[faces[depth[i]][0]];\n\t\tfor(var u = 1; u < faces[depth[i]].length; u++)\n\t\t{\n\t\t\td+= ' ' + 'L'+x_coords[faces[depth[i]][u]]+' '+y_coords[faces[depth[i]][u]];\n\t\t}\n\t\td+= ' Z';\n\t\tface.setAttributeNS(null, 'd', d);\n\t}\n}"
+            
+            
             svgContents += "\n\n\nfunction rotateAboutX(radians)\n{\n\tfor(var i=0; i<x_coords.length; i++)\n\t{\n\t\ty = y_coords[i] - centre_y;\n\t\tz = z_coords[i] - centre_z;\n\t\td = Math.sqrt(y*y + z*z);\n\t\ttheta  = Math.atan2(y, z) + radians;\n\t\ty_coords[i] = centre_y + d * Math.sin(theta);\n\t\tz_coords[i] = centre_z + d * Math.cos(theta);\n\t}\n}"
             svgContents += "\n\n\nfunction rotateAboutY(radians)\n{\n\tfor(var i=0; i<x_coords.length; i++)\n\t{\n\t\tx = x_coords[i] - centre_x;\n\t\tz = z_coords[i] - centre_z;\n\t\td = Math.sqrt(x*x + z*z);\n\t\ttheta  = Math.atan2(x, z) + radians;\n\t\tx_coords[i] = centre_x + d * Math.sin(theta);\n\t\tz_coords[i] = centre_z + d * Math.cos(theta);\n\t}\n\tdrawBox();\n}"
 
@@ -121,59 +136,13 @@ def drawLines(pathToFbx):
             
             svgContents += "\n\n</svg>"
 
-
-    with open('myFile.svg', 'w') as file_:
+    svgFileName = 'SVG' + (pathToFbx[4:])[:-3] + 'svg'        
+    with open(svgFileName, 'w') as file_:
         file_.write(svgContents)
     
 def clamp(x): 
   return max(0, min(x, 255))    
 
-def getVerticesCount(pathToFbx):
-    sdk_manager, scene = FbxCommon.InitializeSdkObjects()
-    
-
-    if not FbxCommon.LoadScene(sdk_manager, scene, os.getcwd()+pathToFbx):
-        print("error in LoadScene. File found : %s" % os.path.isfile(pathToFbx))
-        print("Current working directory : %s" % os.getcwd())
-        print("file directory : %s" % os.getcwd()+pathToFbx)
-        
-    counter = 0
-    lineCounter = 0
-    lineList = ""
-    for u in range(scene.GetNodeCount()):
-        node = scene.GetNode(u)
-        for i in range(node.GetChildCount()):
-            child = node.GetChild(i)
-            counter += child.GetMesh().GetPolygonVertexCount()
-            lineCounter += child.GetMesh().GetMeshEdgeCount()
-            child.GetMesh().BeginGetMeshEdgeVertices()
-            polCOunt = ""
-            for z in range(child.GetMesh().GetMeshEdgeCount()):
-                startIndex = -1
-                endIndex = -1
-                startIndex, endIndex = child.GetMesh().GetMeshEdgeVertices(z)
-                startVector = child.GetMesh().GetControlPointAt(startIndex)
-                endVector = child.GetMesh().GetControlPointAt(endIndex)
-                
-
-
-                polList = "<br/>"
-
-                for m in range(child.GetMesh().GetPolygonCount()):
-                    polList += "("
-                    for n in range(child.GetMesh().GetPolygonSize(m)):
-                        polList += str(child.GetMesh().GetControlPointAt(child.GetMesh().GetPolygonVertex(m,n)))+","
-                    polList += ")<br/>"
-
-
-                lineList += "<br/>" + str(startVector) + " - " + str(endVector)
-            child.GetMesh().EndGetMeshEdgeVertices()
-            attr_type = child.GetNodeAttribute().GetAttributeType()
-
-            if attr_type == FbxCommon.FbxNodeAttribute.eMesh:
-                print(child)
-    return (str(counter) + " edge count " + str(lineCounter) + "-" + polCOunt + polList + lineList )
-    
 
 httpd = BaseHTTPServer.HTTPServer(("localhost", 8000), MyHandler)
 httpd.serve_forever()
